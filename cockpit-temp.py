625ef29070860e8515bef406f8eea936c4731ecb
ishiii-s <94646741+ishiii-s@users.noreply.github.com>
2023-08-07 17:54:29 +0530

    Add files via upload

---
+++ cockpit-temp.py
@@ -0,0 +1,42 @@
+from google.cloud import bigquery
+import json
+
+# Initialize a BigQuery client
+client = bigquery.Client()
+
+# Define a dictionary to store results
+top_apps = {}
+
+# List of SQL queries and corresponding marketplace names
+queries = [
+    ("AWS_MARKETPLACE", "SELECT * FROM `Marketplace_Dataset.AWS_MARKETPLACE` ORDER BY reviews DESC LIMIT 1;"),
+    ("AZURE_MARKETPLACE", "SELECT * FROM `Marketplace_Dataset.AZURE_MARKETPLACE` ORDER BY reviews DESC LIMIT 1;"),
+    ("FigmaPlugins", "SELECT * FROM `Marketplace_Dataset.FigmaPlugins` ORDER BY usage_number DESC LIMIT 1;"),
+    ("GITHUB_TOPICS", "SELECT * FROM `Marketplace_Dataset.GITHUB_TOPICS` ORDER BY stars DESC LIMIT 1;"),
+    ("HerokuSpider", "SELECT * FROM `Marketplace_Dataset.HerokuSpider` ORDER BY stars DESC LIMIT 1;"),
+    ("VISUAL_STUDIO_MARKETPLACE", "SELECT * FROM `Marketplace_Dataset.VISUAL_STUDIO_MARKETPLACE` ORDER BY installs DESC LIMIT 1;")
+    # Add more queries and marketplace names here...
+]
+
+# Loop through each query and execute it
+for marketplace, query in queries:
+    # Execute the query
+    query_job = client.query(query)
+
+    # Fetch the result
+    result = query_job.result()
+
+    # Access the result row
+    row = next(result)
+
+    # Store the result in the dictionary
+    top_apps[marketplace] = {
+        "app_name": row.app_name,
+        "marketplace_name": marketplace
+    }
+
+# Convert the dictionary to JSON format
+json_result = json.dumps({"top-apps": top_apps}, indent=4)
+
+# Print the JSON result
+print(json_result)
